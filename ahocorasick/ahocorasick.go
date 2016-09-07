e15f9d07cd03ccf724fb88ac0663d40c028099e9
fitzr <fitzroy.t.hagi@gmail.com>
2016-09-07 10:50:02 +0900

    Add AhoCorasick test

---
+++ ahocorasick/ahocorasick.go
@@ -0,0 +1,12 @@
+package ahocorasick
+
+type AhoCorasick struct {
+}
+
+func New(keywords []string) *AhoCorasick {
+	return nil
+}
+
+func (a *AhoCorasick) Match(text string) [][]int {
+	return nil
+}
---
+++ ahocorasick/ahocorasick_test.go
@@ -0,0 +1,62 @@
+package ahocorasick
+
+import (
+	"bufio"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestAhoCorasick_Match(t *testing.T) {
+	input := "a his hoge hershe xx."
+	keywords := []string{"he", "hers", "his", "she"}
+	expected := [][]int{
+		{2, 3},  // his
+		{11, 2}, // he
+		{11, 4}, // hers
+		{14, 3}, // she
+		{15, 2}} // he
+	expectMatched := []string{"his", "he", "hers", "she", "he"}
+
+	sut := New(keywords)
+	actual := sut.Match(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("\nexpected: %v\nactual: %v", expected, actual)
+	}
+
+	matched := make([]string, len(actual))
+	for i, v := range actual {
+		matched[i] = input[v[0] : v[0]+v[1]]
+	}
+	if !reflect.DeepEqual(expectMatched, matched) {
+		t.Errorf("\nexpected: %v\nactual: %v", expectMatched, matched)
+	}
+}
+
+func TestAhoCorasick_MatchWithBigData(t *testing.T) {
+	input := "今日は天気がよかったので、近くの海まで愛犬のしなもんと一緒にお散歩。写真は海辺を楽しそうに歩くしなもん。そのあとついでにお買い物にも行ってきました。「はてなの本」を買ったので、はてなダイアリーの便利な商品紹介ツール「はまぞう」を使って紹介してみるよ。とてもおもしろいのでみんなも読んでみてね。"
+	keywords := getKeywords()
+	expectMatched := []string{"今日", "天気", "しなもん", "散歩", "写真", "海辺", "しなもん", "はてな", "はてなの本", "はてな", "はてなダイアリ", "はてなダイアリー", "ダイアリー", "商品", "はまぞう", "おもしろい"}
+
+	sut := New(keywords)
+	actual := sut.Match(input)
+
+	matched := make([]string, len(actual))
+	for i, v := range actual {
+		matched[i] = input[v[0] : v[0]+v[1]]
+	}
+	if !reflect.DeepEqual(expectMatched, matched) {
+		t.Errorf("\nexpected: %v\nactual: %v", expectMatched, matched)
+	}
+}
+
+func getKeywords() []string {
+	fp, _ := os.Open("../data/keyword.utf8.uniq.txt")
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+	keywords := make([]string, 216263)
+	for i := 0; scanner.Scan(); i++ {
+		keywords[i] = scanner.Text()
+	}
+	return keywords
+}
